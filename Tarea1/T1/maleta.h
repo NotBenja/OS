4bb22538ff6fb50d4f3b53255878d7d6f9fa1b63
NotBenja <benjamin.reyes.b@ug.uchile.cl>
2024-03-28 07:06:48 -0300

    avances

--- Tarea1/T1/maleta.h
+++ Tarea1/T1/maleta.h
@@ -1,4 +1,23 @@
-double llenarMaletaPar(double w[], double v[], int z[], int n, double maxW,
-                       int k);
+#include <stdio.h>
+#include <pthread.h>
+
+typedef struct {
+    
+
+} Args;
+
+void *eleccion(void *p){
+
+}
+
 double llenarMaletaSec(double w[], double v[], int z[], int n, double maxW,
                        int k);
+double llenarMaletaPar(double w[], double v[], int z[], int n, double maxW,int k){
+    double best = -1;
+    pthread_t p_id[8];
+    Args *args;
+    for(int i = 0; i<8 ;i++){
+        pthread_create(&p_id[i],NULL, eleccion, args);
+    }
+}
+ 
