4e3a384e117d3accba242d4cebf8d44f817602e4
NotBenja <benjamin.reyes.b@ug.uchile.cl>
2024-04-24 09:41:44 -0400

    Avances pequeños

--- Tarea3/T3/disk.c
+++ Tarea3/T3/disk.c
@@ -11,21 +11,32 @@
  * Agregue aca los tipos, variables globales u otras
  * funciones que necesite
  *****************************************************/
+PriQueue *q;
+pthread_mutex_t m;
 
-...
 
+//Estructura para requests: request
+//Estructura para ordenar request:PriQueue
 void iniDisk(void) {
-  ...
+  pthread_mutex_init(&m,NULL);
+  q = makePriQueue();
 }
 
 void cleanDisk(void) {
-  ...
+  pthread_mutex_destroy(&m);
+  destroyPriQueue(q);
 }
 
 void requestDisk(int track) {
-  ...
+  pthread_mutex_lock(&m);
+
+
+  pthread_mutex_unlock(&m);
 }
 
 void releaseDisk() {
-  ...
+  pthread_mutex_lock(&m);
+
+
+  pthread_mutex_unlock(&m);
 }
