a8c595a4e03aac4458089dfedbbb2470296c65c4
NotBenja <benjamin.reyes.b@ug.uchile.cl>
2024-05-28 22:12:38 -0400

    T5 terminada

--- Tarea5/T5/rwlock.c
+++ Tarea5/T5/rwlock.c
@@ -11,6 +11,17 @@ struct rwlock {
   NthQueue *pending_writers;
 };
 
+// Funcion que se llama cuando se termina el timeout
+void nDeleteFromQueue(nThread th){
+  // Se llama a la cola donde esta el nThread 
+  NthQueue *queue = th->ptr;
+  // Borramos el nThread de la cola
+  nth_delQueue(queue, th);
+  // Quitamos la cola del descriptor del nThread
+  th->ptr = NULL;
+}
+
+
 nRWLock *nMakeRWLock() {
   // Pedimos memoria para la estructura
   nRWLock *rwl = malloc(sizeof(nRWLock));
@@ -58,8 +69,23 @@ int nEnterWrite(nRWLock *rwl, int timeout) {
     // Si no, encolamos la solicitud del escritor y esperamos
     nThread this_th = nSelf();
     nth_putBack(rwl->pending_writers, this_th);
-    suspend(WAIT_RWLOCK);
-    schedule();
+    // Si el timeout es <=0, esperamos indefinidamente
+    if (timeout <= 0) {
+      suspend(WAIT_RWLOCK);
+      schedule();
+     
+    } else {
+      // Si no, esperamos el tiempo del timeout con nth_programTimer
+      suspend(WAIT_RWLOCK_TIMEOUT);
+      this_th->ptr = (NthQueue*) rwl->pending_writers;
+      // En caso de exceder el tiempo, llamamos a nDeleteFromQueue que borra el nThread de la cola
+      nth_programTimer(timeout*1000000LL, nDeleteFromQueue);
+      schedule();
+      // Si despues de llamar a schedule() el descriptor del nThread no tiene a la cola, entonces retornamos 0
+      if(this_th->ptr == NULL){
+        return 0;
+      }
+    }
   }
   // Termina seccion critica
   END_CRITICAL
@@ -74,10 +100,15 @@ void nExitRead(nRWLock *rwl) {
   // Si no hay lectores y hay escritores pendientes, se despierta al siguiente escritor
   if (rwl->readers == 0 && !nth_emptyQueue(rwl->pending_writers)) {
     nThread th = nth_getFront(rwl->pending_writers);
+    // Si el escritor esta esperando, cancelamos su timer
+    if(th->status == WAIT_RWLOCK_TIMEOUT){
+      nth_cancelThread(th);
+    }
     // Se cambia el contexto a escribiendo
     rwl->writing = 1;
     setReady(th);
     schedule();
+
   }
  // Termina seccion critica
   END_CRITICAL
@@ -101,6 +132,10 @@ void nExitWrite(nRWLock *rwl) {
     // Si no hay lectores pendientes, se revisa si hay escritores pendientes
   } else if (!nth_emptyQueue(rwl->pending_writers)) {
     nThread th = nth_getFront(rwl->pending_writers);
+    // Si el escritor esta esperando, cancelamos su timer
+    if(th->status == WAIT_RWLOCK_TIMEOUT){
+      nth_cancelThread(th);
+    } 
     // Se cambia el contexto a escribiendo
     rwl->writing = 1;
     // Despertamos al escritor
