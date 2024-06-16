cb98bcc9fb059fca80489e349103290aa05e0736
NotBenja <benjamin.reyes.b@ug.uchile.cl>
2024-06-16 19:52:11 -0400

    Enunciado T6

---
+++ Tarea6/T6/bolsa.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "pss.h"
+#include "bolsa.h"
+#include "spinlocks.h"
+
+// Declare aca sus variables globales
+
+int vendo(int precio, char *vendedor, char *comprador) {
+  ...
+}
+
+int compro(char *comprador, char *vendedor) {
+  ...
+}
